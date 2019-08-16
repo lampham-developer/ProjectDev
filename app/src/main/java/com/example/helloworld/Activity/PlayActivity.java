3d898fd5c78687a8ece5bb317f6b42139fc577d5
lampham250696 <52642341+lampham250696@users.noreply.github.com>
2019-08-16 23:01:05 +0700

    lam

--- app/src/main/java/com/example/helloworld/Activity/PlayActivity.java
+++ app/src/main/java/com/example/helloworld/Activity/PlayActivity.java
@@ -1,24 +1,27 @@
 package com.example.helloworld.Activity;
 
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.media.AudioManager;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.helloworld.Adapter.SuggestVideoAdapter;
 import com.example.helloworld.Entity.Define;
@@ -47,6 +50,7 @@ import java.util.List;
 public class PlayActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    ActionBar actionbar;
     Video video;
     PlayerView playerView;
     ExoPlayer exoPlayer;
@@ -55,12 +59,16 @@ public class PlayActivity extends AppCompatActivity {
     String json;
     List<Video> videoList;
     String category = "null";
-    String url ;
+    String url;
     CallAPI callAPI;
     ProgressBar pb_suggest_video;
     ImageButton bt_full_screen;
     int screen_sate;
+    SuggestVideoAdapter videoAdapter;
 
+    AudioManager audioManager;
+    GestureDetector gestureDetector;
+    long currentPosition;
 
 
     @Override
@@ -74,33 +82,47 @@ public class PlayActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.rv_suggest);
         tv_suggest_video = findViewById(R.id.tv_suggest_video);
         playerView = findViewById(R.id.pv_playing_video);
+        audioManager =(AudioManager) getSystemService(Context.AUDIO_SERVICE);
         toolbar = findViewById(R.id.toolbar_playing);
+
+
+        gestureDetector = new GestureDetector(getBaseContext(), new MyGesture());
         setSupportActionBar(toolbar);
 
-        ActionBar actionbar = getSupportActionBar();
+        actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_arrow_back_white);
-
+        screen_sate = Define.WINDOW_SCREEN;
         bt_full_screen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(screen_sate == Define.FULL_SCREEN){
+                if (screen_sate == Define.FULL_SCREEN) {
                     setWindowScreen();
-                }else{
+                } else {
                     setFullScreen();
                 }
             }
         });
+
+
+        playerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                gestureDetector.onTouchEvent(motionEvent);
+                return true;
+            }
+        });
+
         video = (Video) getIntent().getSerializableExtra("video");
         url = getIntent().getStringExtra("url");
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
         startPlayVideo(video);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case android.R.id.home :
+        switch (item.getItemId()) {
+            case android.R.id.home:
                 stopPlayVideo();
                 finish();
                 return true;
@@ -108,32 +130,33 @@ public class PlayActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void startPlayVideo(Video video){
+    private void startPlayVideo(Video video) {
         stopPlayVideo();
         recyclerView.setAdapter(null);
         new VideoHTTP(url).execute();
         setTitle(video.getTitle());
-        String url = video.getMp4_url();
+        String video_url = video.getMp4_url();
         TrackSelector trackSelector = new DefaultTrackSelector();
         LoadControl loadControl = new DefaultLoadControl();
         RenderersFactory renderersFactory = new DefaultRenderersFactory(getBaseContext());
 
         exoPlayer = ExoPlayerFactory.newSimpleInstance(renderersFactory, trackSelector, loadControl);
         String userAgent = Util.getUserAgent(getBaseContext(), getString(R.string.app_name));
-        Uri uri = Uri.parse(url);
+        Uri uri = Uri.parse(video_url);
 
         MediaSource mediaSource = new ExtractorMediaSource.Factory(new DefaultDataSourceFactory(getBaseContext(), userAgent)).setExtractorsFactory(new DefaultExtractorsFactory()).createMediaSource(uri);
+
 
         playerView.setFitsSystemWindows(true);
         exoPlayer.prepare(mediaSource);
         exoPlayer.setPlayWhenReady(true);
         playerView.setPlayer(exoPlayer);
-
     }
 
-    private void setFullScreen(){
+    private void setFullScreen() {
         bt_full_screen.setImageResource(R.drawable.ic_fullscreen_skrink);
-        RelativeLayout.LayoutParams params =(RelativeLayout.LayoutParams) playerView.getLayoutParams();
+        getSupportActionBar().hide();
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) playerView.getLayoutParams();
         params.height = params.MATCH_PARENT;
         params.width = params.MATCH_PARENT;
         playerView.setLayoutParams(params);
@@ -141,9 +164,12 @@ public class PlayActivity extends AppCompatActivity {
         playerView.setResizeMode(AspectRatioFrameLayout.RESIZE_MODE_FILL);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
-    private void setWindowScreen(){
+
+    private void setWindowScreen() {
         bt_full_screen.setImageResource(R.drawable.ic_fullscreen_expand);
-        RelativeLayout.LayoutParams params =(RelativeLayout.LayoutParams) playerView.getLayoutParams();
+        recyclerView.setAdapter(videoAdapter);
+        getSupportActionBar().show();
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) playerView.getLayoutParams();
         params.height = params.WRAP_CONTENT;
         params.width = params.MATCH_PARENT;
         playerView.setLayoutParams(params);
@@ -152,28 +178,24 @@ public class PlayActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
 
-        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE){
-        }
-        if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
-        }
-    }
-
-    private void stopPlayVideo(){
+    private void stopPlayVideo() {
         try {
             exoPlayer.stop();
             exoPlayer.release();
-        }catch (Exception e){
+        } catch (Exception e) {
 
         }
     }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
 
     class VideoHTTP extends AsyncTask<String, Void, String> {
         String url;
+
         public VideoHTTP(String url) {
             this.url = url;
         }
@@ -195,11 +217,11 @@ public class PlayActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            if(json != null){
+            if (json != null) {
                 tv_suggest_video.setText("");
                 pb_suggest_video.setVisibility(View.INVISIBLE);
                 videoList = callAPI.getListVideo(json, category, true);
-                SuggestVideoAdapter videoAdapter = new SuggestVideoAdapter(videoList, getBaseContext(), new VideoClick() {
+                videoAdapter = new SuggestVideoAdapter(videoList, getBaseContext(), new VideoClick() {
                     @Override
                     public void onClick(Video video) {
                         startPlayVideo(video);
@@ -207,11 +229,42 @@ public class PlayActivity extends AppCompatActivity {
                 }, video);
                 recyclerView.setAdapter(videoAdapter);
                 recyclerView.setLayoutManager(new LinearLayoutManager(getBaseContext(), RecyclerView.VERTICAL, false));
-            }else{
+            } else {
                 pb_suggest_video.setVisibility(View.INVISIBLE);
                 tv_suggest_video.setText(getString(R.string.disconnect));
             }
         }
 
     }
+
+    class MyGesture extends GestureDetector.SimpleOnGestureListener{
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            //trai qua phai
+            if (e2.getX() - e1.getX() > Define.SWIPE_THRESHOLD && Math.abs(velocityX) > Define.SWIPE_VELO_THRESHOLD){
+                currentPosition = exoPlayer.getCurrentPosition();
+                long distance = (long) (e2.getX() - e1.getX());
+                playerView.showController();
+                exoPlayer.seekTo(currentPosition + distance*100);
+            }
+            //phai qua trai
+            if (e1.getX() - e2.getX() > Define.SWIPE_THRESHOLD && Math.abs(velocityX) > Define.SWIPE_VELO_THRESHOLD){
+                currentPosition = exoPlayer.getCurrentPosition();
+                long distance = (long) (e1.getX() - e2.getX());
+                playerView.showController();
+                exoPlayer.seekTo(currentPosition - distance*100);
+            }
+            //tren xuong duoi
+            if (e2.getY() - e1.getY() > Define.SWIPE_THRESHOLD && Math.abs(velocityY) > Define.SWIPE_VELO_THRESHOLD){
+
+            }
+            //duoi len tren
+            if (e1.getY() - e2.getY() > Define.SWIPE_THRESHOLD && Math.abs(velocityY) > Define.SWIPE_VELO_THRESHOLD){
+
+            }
+
+            return super.onFling(e1, e2, velocityX, velocityY);
+        }
+    }
+
 }
--- app/src/main/java/com/example/helloworld/Entity/Define.java
+++ app/src/main/java/com/example/helloworld/Entity/Define.java
@@ -4,6 +4,8 @@ public class Define {
 
     public static final int FULL_SCREEN = 0;
     public static final int WINDOW_SCREEN = 1;
+    public static final int SWIPE_THRESHOLD = 10;
+    public static final int SWIPE_VELO_THRESHOLD = 1;
 
 
     public static final String HOT_VIDEO_URL = "https://demo5639557.mockable.io/getVideoHot";
